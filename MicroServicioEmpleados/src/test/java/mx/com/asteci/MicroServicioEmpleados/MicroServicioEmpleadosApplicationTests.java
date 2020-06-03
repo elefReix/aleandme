efb89eb867f6ced6d8c5a6cde0f70779f32ea031
ale <alejandraortizvazquez19@gmail.com>
2020-06-03 13:27:37 -0500

    Servicio de tipo Rest contiene documentacion swagger

---
+++ MicroServicioEmpleados/src/test/java/mx/com/asteci/MicroServicioEmpleados/MicroServicioEmpleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.asteci.MicroServicioEmpleados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicioEmpleadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
