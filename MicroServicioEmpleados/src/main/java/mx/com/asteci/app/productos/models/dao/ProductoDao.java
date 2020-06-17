5e34b3065703b93e3291fb177c71cf3bdfc5a4b4
ale <alejandraortizvazquez19@gmail.com>
2020-06-17 16:16:42 -0500

    json de un microServicio

--- MicroServicioEmpleados/src/main/java/mx/com/asteci/app/productos/models/dao/ProductoDao.java
+++
@@ -1,9 +0,0 @@
-package mx.com.asteci.app.productos.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import mx.com.asteci.app.productos.models.entity.Producto;
-
-public interface ProductoDao extends CrudRepository<Producto, Long>{
-
-}
